0eff78982624bb9dbdfd39fcca6ad92cadc5ffb5
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-05-14 20:32:16 +0500

    about page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 <h3><a href="/">PORTFOLIO</a></h3>
 <ul>
   <li><a href="/">Home</a></li>
-  <li className="navBtn"><a href="/">About</a></li>
+  <li className="navBtn"><a href="#about">About</a></li>
   <li className="navBtn"><a href="/">Blogs</a></li>
   <li className="navBtn"><a href="/">Contact</a></li>
   <li className="navBtn"><a href="/">Resume</a></li>
@@ -24,11 +24,38 @@ export default function Home() {
 <a href="https://wa.me/+923096946556"><button className="hireBtn">Hire Me</button></a>
   </div>
 </div>
-<div className="about">
+<div className="seperator"></div>
+<div className="about" id="about">
 <div className="content">
   <h1>About Me</h1>
+  <p>I have created responsive and user-friendly websites using modern front-end technologies. Leveraging my knowledge of HTML, CSS, and JavaScript, I have implemented intuitive user interfaces, engaging animations, and seamless navigation. I am well-versed in popular front-end frameworks such as Bootstrap and Foundation. I have utilized these frameworks to streamline development processes, enhance cross-browser compatibility, and ensure mobile responsiveness.</p>
+<br/>
+<p className="aboutHeading2">Technologies I've worked with:</p>
+<br/>
+<div className="tech">
+  <p>Html</p>
+  <p className="col2">React Js, Next Js</p>
 </div>
-<div className="pic"></div>
+<div className="tech">
+  <p>CSS</p>
+  <p className="col2">Bootstrap, Material UI</p>
+</div>
+<div className="tech">
+  <p>Javascript</p>
+  <p className="col2">Node Js</p>
+</div>
+<div className="tech">
+  <p>Typescript</p>
+  <p className="col2">VS Code</p>
+</div>
+<div className="tech">
+  <p>Github</p>
+  <p className="col2">Vercel, Netlify</p>
+</div>
+</div>
+<div className="pic">
+  <img src="/images/aboutimg.png"></img>
+</div>
 </div>
     </div>
   )
